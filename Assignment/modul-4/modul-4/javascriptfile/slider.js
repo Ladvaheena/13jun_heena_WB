26f445ccfa5bba419ca9710791fc93ff13a354ee
Ladvaheena <136426430+Ladvaheena@users.noreply.github.com>
2024-01-03 10:43:01 +0530

    Update slider.js

--- Assignment/modul-4/modul-4/javascriptfile/slider.js
+++ Assignment/modul-4/modul-4/javascriptfile/slider.js
@@ -1,14 +1,21 @@
 let slide = document.querySelectorAll('.box');
         var counter = 0;
-       
-        // console.log(slide)
         let imageslider = () => {
+            if(counter > 2){
+                counter = 2
+            }
+            else if(counter < -2)
+            {
+                counter = -2;
+            }
             slide.forEach (
                 (slide) => {
                     slide.style.transform = `translateX(${counter* 120}%)`
                 }
             )
         }
+        let imageslider = () => {
+          if(count
         function goback(){
             counter --;
             imageslider();
