a9777a27970401aa57c6bb989b59131e8459e10a
Ladvaheena <136426430+Ladvaheena@users.noreply.github.com>
2024-02-08 12:32:32 +0530

    08-02-2024

---
+++ project/megazzine/javascriptfile/contact.js
@@ -0,0 +1,13 @@
+function menu(){
+    let sublist = document.getElementById('sublist');
+   if(sublist.style.display == 'block')
+     {
+         sublist.style.display = 'none';
+     }
+     else{
+         sublist.style.display = 'block';
+     }
+}
+function enter() {
+    confirm('Thank you for writing us..')
+  }
