694530187698b8441b59cadf3f329e271746c730
Ladvaheena <136426430+Ladvaheena@users.noreply.github.com>
2024-02-19 11:50:02 +0530

    19-2-2024

---
+++ project/megazzine/javascriptfile/home.js
@@ -0,0 +1,10 @@
+function menu(){
+    let sublist = document.getElementById('sublist');
+   if(sublist.style.display == 'block')
+     {
+         sublist.style.display = 'none';
+     }
+     else{
+         sublist.style.display = 'block';
+     }
+}
